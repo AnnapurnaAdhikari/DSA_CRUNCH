23808ae116f2e52901745d8631ff9c70a456f7fb
Anna <adhikariannapurna2201@gmail.com>
2026-05-02 01:14:35 +0530

    added optimal approach to 84

--- Stacks/LarRectHist.cpp
+++ Stacks/LarRectHist.cpp
@@ -17,3 +17,27 @@ int largestRectangleArea0(vector<int>& heights) {
     }
     return maxAr;
 }
+
+// Optimal Approach using STACK(MONOTONIC)
+int largestRectangleArea1(vector<int>& heights){
+    int n = heights.size();
+    stack<pair<int, int>> stk; //{idx, ht}
+    int maxAr = 0;
+
+    for(int i = 0; i < n; i++){
+        int start = i;
+        while(!stk.empty() && stk.top().second > heights[i]){
+            auto [idx, ht] = stk.top();
+            stk.pop();
+            maxAr = max(maxAr, ht*(i-idx));
+            start = idx;
+        }
+        stk.push({start, heights[i]});
+    }
+    while(!stk.empty()){
+        auto [idx, ht] = stk.top();
+        stk.pop();
+        maxAr = max(maxAr, ht*(n-idx));
+    }
+    return maxAr;
+}

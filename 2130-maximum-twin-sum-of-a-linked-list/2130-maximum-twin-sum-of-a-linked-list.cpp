d61a8e00b724f9bdd3bd38218e79638624e9b382
Annapuna Adhikari <159609823+AnnapurnaAdhikari@users.noreply.github.com>
2026-06-14 15:24:37 +0530

    Time: 12 ms (11.39%), Space: 124.3 MB (85.28%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -10,21 +10,40 @@
  */
 class Solution {
 public:
+    ListNode* midLL(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* reversedLL(ListNode* head){
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* temp = NULL;
+
+        while(curr){
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr= temp;
+        }
+        return prev;
+    }
+
     int pairSum(ListNode* head) {
-        vector<int>nodes;
-        while(head){
-            nodes.push_back(head->val);
+        ListNode* curr = reversedLL(midLL(head));
+        int maxSum = -1;
+        while(curr){
+            maxSum = max(maxSum, curr->val + head->val);
+            curr= curr->next;
             head = head->next;
         }
-        int maxSum = 0;
-        int currSum = 0;
-        int n = nodes.size();
-        for(int i = 0; i <= (n/2 -1); i++){
-            currSum = nodes[i] + nodes[n-i-1];
-            // cout<<"curr: "<<currSum;
-            maxSum = max(maxSum, currSum);
-        }
-        // cout<<"Max: "<<maxSum;
         return maxSum;
+        
     }
 };

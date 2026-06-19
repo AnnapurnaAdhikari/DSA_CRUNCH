ef31cb780ed2eb4fb5ab6e0b91d836b3e0491316
Annapuna Adhikari <159609823+AnnapurnaAdhikari@users.noreply.github.com>
2026-06-19 12:38:13 +0530

    Time: 7 ms (0.76%), Space: 11.7 MB (10.95%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -3,12 +3,15 @@ public:
     int largestAltitude(vector<int>& gain) {
         int n = gain.size();
         vector<int> alt(n+1, 0);
+        int maxi = 0;
 
         for(int i = 0; i < n; i++){
             alt[i+1] = alt[i] + gain[i];
             cout<<alt[i+1]<<" ";
+            maxi = max(maxi, alt[i+1]);
         }
-        auto maxAlt = max_element(alt.begin(), alt.end());
-        return *maxAlt;
+        // auto maxAlt = max_element(alt.begin(), alt.end());
+        // return *maxAlt;
+        return maxi;
     }
 };

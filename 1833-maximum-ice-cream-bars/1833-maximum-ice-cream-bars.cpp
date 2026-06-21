ca9d6644517e1299e1862e29cd99d85fa6a5bac1
Annapuna Adhikari <159609823+AnnapurnaAdhikari@users.noreply.github.com>
2026-06-21 23:39:29 +0530

    Time: 0 ms (100%), Space: 84.3 MB (25.28%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(), costs.end());
-        int money = 0;
-        int max_ice = 0;
-        for(int cost: costs){
-            if(money < coins && cost <= coins-money ){
-                money += cost;
-                max_ice++;
-            }
+        // sort(costs.begin(), costs.end());
+        // int money = 0;
+        // int max_ice = 0;
+        // for(int cost: costs){
+        //     if(money < coins && cost <= coins-money){
+        //         money += cost;
+        //         max_ice++;
+        //     }
+        // }
+        // return max_ice;
+
+        //appproach2: counting array
+        int maxCost = *max_element(costs.begin(), costs.end());
+
+        vector<int> freq(maxCost + 1, 0);
+
+        for (int cost : costs) {
+            freq[cost]++;
         }
-        return max_ice;
+
+        int bars = 0;
+
+        for (int cost = 1; cost <= maxCost; cost++) {
+            if (freq[cost] == 0) continue;
+
+            int canBuy = min(freq[cost], coins / cost);
+
+            bars += canBuy;
+            coins -= canBuy * cost;
+
+            if (coins < cost) break;
+        }
+
+        return bars;
         
     }
 };

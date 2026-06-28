31e9b6a21807cfa8cb1cfcd463caf3b5d7e087ee
Annapuna Adhikari <159609823+AnnapurnaAdhikari@users.noreply.github.com>
2026-06-28 13:38:59 +0530

    Time: 56 ms (76.24%), Space: 119.7 MB (71.63%) - LeetHub

---
+++ 3301-maximize-the-total-height-of-unique-towers/3301-maximize-the-total-height-of-unique-towers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maximumTotalSum(vector<int>& maxHt) {
+        sort(maxHt.begin(), maxHt.end(), greater<int>());
+        long long sum = 0;
+        int prev = INT_MAX;
+        for(int h: maxHt){
+            int curr = min(h, prev-1);
+            if(curr == 0)return -1;
+            sum+=curr;
+            prev = curr;
+        }
+        return sum;
+    }
+};

6627bbc5347cf32766ee4d8d5f793f9e9e597b90
Annapuna Adhikari <159609823+AnnapurnaAdhikari@users.noreply.github.com>
2026-09-04 00:36:19 +0530

    Time: 4 ms (65.18%), Space: 165.7 MB (87.43%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        vector<int> n2;
         int minOdd = INT_MAX;
         for(int x: nums1){
             if(x % 2) minOdd = min(minOdd, x);
         }
+        // for even elements, if there exists an odd no great than it then it is convertible to an odd num
+        // if there exists even no. lesser than min odd no present in the array then not possible to make n2
         for(int x: nums1){
             if(x % 2 == 0 && minOdd != INT_MAX && x < minOdd) return false;
         }
